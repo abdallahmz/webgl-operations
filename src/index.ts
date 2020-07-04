ad49c0877df58edbada926b7b873067b9700fd75
ab6d <55534657+ab6d@users.noreply.github.com>
2020-07-04 12:57:42 +0200

    feat: adds typescript + webpack

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+// add styles
+import './style.css';
+// three.js
+import * as THREE from 'three';
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
